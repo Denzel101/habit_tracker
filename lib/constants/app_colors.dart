3d8c8184935718f9d8edee6e5782105ed67c5038
Denzel Gatugu <denzelgatugu@outlook.com>
2025-01-07 20:37:17 +0300

    Created profile screen ui

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color textGrey = Color(0xFF8B8B8B);
   static const Color activeColor = Color(0xFFFD5B32);
   static const Color greyColor = Color(0xFFE1E5F0);
+  static const Color secondaryActiveColor = Color(0xFFFDF8F5);
 }
