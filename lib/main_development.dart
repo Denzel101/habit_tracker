bcfe159893d9fccd6dc4a931baf461bd1fa58b73
Denzel Gatugu <denzelgatugu@outlook.com>
2025-01-08 21:52:31 +0300

    Configured app global values

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,11 +4,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:habit_tracker/app/app.dart';
 import 'package:habit_tracker/bootstrap.dart';
+import 'package:habit_tracker/constants/constants.dart';
 import 'package:habit_tracker/firebase_options.dart';
 import 'package:habit_tracker/utils/utils.dart';
+import 'package:habit_tracker/versioning/build_version.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  HabitTrackerConfig(
+    values: HabitTrackerValues(
+      hiveBoxKey: 'ht_development_$packageVersion',
+      hiveBoxEncryptionKey: 'JxjJXBiUe9JLNk8FIIT0qSO2WQhzlqxj',
+      buildFlavour: BuildFlavour.development,
+    ),
+  );
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
